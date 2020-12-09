b4680c9e406639895bbf65aa08e910c0b2a95e57
shoog <shoogeh.jian@gmail.com>
2020-12-09 18:45:30 +0200

    s

--- Test_Practice.py
+++ Test_Practice.py
@@ -1,5 +1,14 @@
 import Nyx_Utilities as nyx
+import pytest
+
+filepath2 = r"C:\Users\shoog\PycharmProjects\Jian\build_log_nrf52_ble_s112_10040_boot.txt"
+filepath3 = r"C:\Users\shoog\PycharmProjects\Jian\build_log_nrf52_ble_s112_10040_boo.txt"
 
 
 def test_1():
-    nyx.find_last_modified(r"C:\Users\shoog\downloads")
+    assert nyx.Nyx_File_Parse_Utils.check_if_path_exists(filepath2)
+
+
+@pytest.mark.xfail(strict=True)
+def test_2():
+    assert not (nyx.Nyx_File_Parse_Utils.check_if_path_exists(filepath3))
